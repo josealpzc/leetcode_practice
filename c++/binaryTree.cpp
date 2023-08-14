5043052fc919cfcaf64d7e597feeb8462bc6b491
Jose Lopez <j.antonio_lpz@hotmail.com>
2023-08-14 02:09:35 -0500

    Adding binaryTree file to do some testing with structs and pointers

--- c++/binaryTree.cpp
+++ c++/binaryTree.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
+	
+	struct Node          //binary tree node declaration 
+	{
+		Node *left;
+		Node *right;
+		int data;	
+	};
+
+	struct 
+	{
+		int myNum;
+		string myString;
+	}testing;
+	
+	testing.myNum=1;
+	testing.myString= "Hello World!";
+	
+	cout << testing.myNum << "\n";
+	
+	string food = "Pizza";
+	cout << food << " <- Value\n";
+	cout << &food << " <- Address\n";
+
+	string* ptr =&food;
+
+	cout << ptr << " <- Pointer\n";	
+
 	cout << "Hello, World!.\n";
+	
 	return 0;
 
 }
